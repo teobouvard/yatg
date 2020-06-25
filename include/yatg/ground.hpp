885ab178843f5b3a77f87ff35f11460d83338a84
Téo Bouvard <teobouvard@gmail.com>
2020-06-25 20:47:16 +0200

    Basic functionality working
    Display ground plane + camera controller

---
+++ include/yatg/ground.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <QObject>
+
+class Ground : public QComponent
+{
+  Q_OBJECT
+public:
+  Ground();
+};
+
---
+++ src/ground.cpp
@@ -0,0 +1,6 @@
+#include "../include/yatg/ground.hpp"
+
+Ground::Ground()
+{
+
+}
